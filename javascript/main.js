068465cbe77a1833a8e07f489c2ece613c060087
aliamdevs <aliamdevs@gamil.com>
2025-09-11 03:28:45 +0330

    update esenssenal

--- javascript/main.js
+++
@@ -1,8 +0,0 @@
-const { LightHuffmanEncode } = require("./Light-huffman");
-const t= "Today is a sunny day. Let's go out and play, play, play! Today is a sunny day. Come and play with me! Sunny, sunny, sunny, sunny."
-const e = LightHuffmanEncode(t);
-console.log(t.length );
-
-console.log('====================================');
-console.log(e.data.length);
-console.log('====================================');
